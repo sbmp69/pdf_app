4de00692aaac59e821d4ea8e66bb4f74f80439f3
Meet <ajudiameet11@gmail.com>
2026-09-07 15:21:46 +0530

    fix: change FlutterActivity to FlutterFragmentActivity to fix App Lock biometric prompt

--- android/app/src/main/kotlin/com/example/scanproai/scanpro_ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/scanproai/scanpro_ai/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.scanproai.scanpro_ai
 
 import android.content.Intent
 import android.net.Uri
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     private val CHANNEL = "app.channel.shared.data"
     
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
